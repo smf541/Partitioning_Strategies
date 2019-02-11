772c439a1a8c7a03249dd9610490af5c2f7bd4a5
Stella F <stelfelnet@unitybox.de>
2019-02-11 15:41:00 +0000

    updates 11.02.19

--- Partitioning Strategies R files/CharacterNumberVectors.R
+++ Partitioning Strategies R files/CharacterNumberVectors.R
@@ -5,28 +5,28 @@
 
 
 #read in data
-input.dataset <- "./HyolithCharacters.csv" #***change file to needed data set in .csv format***
+input.dataset <- "./SyllidaeCharacters.csv" #***change file to needed data set in .csv format***
 
 data.set <- read.csv(input.dataset)
 
 
 #get partition names
-partition.levels <- levels(data.set$body.region)  #***change $column name***
+partition.levels <- levels(data.set$neotrans)  #***change $column name***
 
 #get vector of character no. for each partition name
 
 get.char.vector <- function(level) {
-  data.set$char.no[data.set$body.region==level] #***change $column name***
+  data.set$char.no[data.set$neotrans==level] #***change $column name***
 }
 
 
 #output as array
 char.vector.array <- as.array(sapply(partition.levels, 
-       FUN=get.char.vector))
+                                     FUN=get.char.vector))
 
 
 #print results
-print(char.vector.array[8])
+print(char.vector.array)
 
 
 
