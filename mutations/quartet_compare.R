c0591ece62899d2f14b8c01912bbb670e2d00deb
Stella F <stelfelnet@unitybox.de>
2019-04-07 17:55:58 +0100

    updates 07/04/2019 #7

--- mutations/quartet_compare.R
+++ mutations/quartet_compare.R
@@ -3,12 +3,12 @@
 require(ape)
 require(phytools)
 require(Quartet)
-setwd("C:/Users/dxsb43/GitHub/Partitioning_Strategies/mutations")
-startTree <- read.nexus("./CEA/Randomized Trees/CEA_TBR_chain3.nex")
-conTree1 <- read.nexus("./CEA/CEA_TBRch_3.nex.tree1.con.tre")
-conTree2 <- read.nexus("./CEA/CEA_TBRch_3.nex.tree2.con.tre")
-conTree3 <- read.nexus("./CEA/CEA_TBRch_3.nex.tree3.con.tre")
-conTree4 <- read.nexus("./CEA/CEA_TBRch_3.nex.tree4.con.tre")
+setwd("C:/local/dxsb43/GitHub/Partitioning_Strategies/mutations")
+startTree <- read.nexus("./CEA/Randomized Trees/CEA_TBR_chain5.nex")
+conTree1 <- read.nexus("./CEA/CEA_TBRch_5.nex.tree1.con.tre")
+conTree2 <- read.nexus("./CEA/CEA_TBRch_5.nex.tree2.con.tre")
+conTree3 <- read.nexus("./CEA/CEA_TBRch_5.nex.tree3.con.tre")
+conTree4 <- read.nexus("./CEA/CEA_TBRch_5.nex.tree4.con.tre")
 trees <- list(startTree, conTree1, conTree2, conTree3, conTree4)
 
 
