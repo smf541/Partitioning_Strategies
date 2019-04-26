b83bf6c60f9b83403538e2584638b28c7c9efd02
Stella F <stelfelnet@unitybox.de>
2019-04-26 13:28:05 +0100

    updates 26/04/19 #17

---
+++ mutations/OZL/ShellScripts/OZL_NNI_chain_12.sh
@@ -0,0 +1,40 @@
+#!/bin/csh
+
+## By default SLURM combines the standard output and error streams in a single
+## file based on the jobID and with extension .out
+## These streams can be directed to separate files with these two directives
+
+#SBATCH  -o  %j.out
+#SBATCH  -e  %j.err
+
+## where SLURM will expand %j to the jobID of the job.
+
+
+
+## Request email to the user when certain type of events occur to
+## the job
+
+#SBATCH  --mail-type=FAIL
+
+## where <type> can be one of BEGIN, END, FAIL, REQUEUE or ALL,
+## and send to email address
+
+#SBATCH  --mail-user  stella.m.felsinger@durham.ac.uk
+#SBATCH -t 03:00:00
+##form     hh:mm:ss
+##SBATCH --exclude cn7051
+## The default email name is that of the submitting user as known to the system.
+
+
+module purge
+module load slurm/current
+
+##load any modules required here
+
+module load mrbayes/ompi/gcc/3.2.6
+
+
+##execute the MPI program
+
+mpirun -n $SLURM_NTASKS mb /ddn/data/dxsb43/mutateOZL/OZLNNI_chain/OZL_NNI_chain.nex.12.nex
+
